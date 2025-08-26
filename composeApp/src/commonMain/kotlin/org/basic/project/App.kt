256b997cea334f1532ef82b3fdb1f1c95dc97996
juanmaaidar <juanmanuelaidar@gmail.com>
2025-08-26 17:37:55 -0300

    Update App.kt
    This commit updates the `App.kt` file to include a `TextField` for user input. It also displays a greeting message with the entered name and an animated version of the same message.

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -1,28 +1,25 @@
 package org.basic.project
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row // Added import for Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height // Added import
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kmpbasico.composeapp.generated.resources.Res
-import kmpbasico.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.unit.dp // Added import
+import androidx.compose.ui.unit.sp // Added import
 
 @Composable
 fun App() {
     MaterialTheme {
+        var name: String by remember { mutableStateOf("") }
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -32,15 +29,30 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally
 
         ) {
-            Text(
-                text = "Hola mundo"
+            TextField(
+                value = name,
+                onValueChange = {
+                    name = it
+                }
             )
-            Text(
-                text = "Programacion 2"
-            )
-            Text(
-                text = "Android Studio"
-            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            if (name.isNotEmpty()) {
+                Text(
+                    text = "Ingreso de $name",
+                    fontSize = 24.sp // Corrected parameter name
+                )
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            AnimatedVisibility(name.isNotEmpty()){
+                Text(
+                    text = "Version animada de $name", // Added comma
+                    fontSize = 24.sp // Corrected parameter name
+                )
+            }
         }
     }
 }
